7947e4563ec0ce82945cbbdeccc2de1c8c5f7288
TimurMeneshev <timmyphone@icloud.com>
2023-02-19 20:02:27 +0700

    practice

--- language-basics/Employee.java
+++ language-basics/Employee.java
@@ -1,20 +1,21 @@
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class Employee {
-    private String name;
+public class Employee extends Person {
     private int salary;
     private LocalDate hireDate;
 
     public Employee(String name, int salary, LocalDate hireDate) {
-        Objects.requireNonNull(name, "Name cannot be null"); // обработка null
-        this.name = name;
-        //this.name = Objects.requireNonNullElse(name, "unknown");
+        super(name);
         this.salary = salary;
         this.hireDate = hireDate;
     }
 
-    public String getName() { return this.name; }
+    @Override
+    public String getDescription() {
+        return String.format("an employee with a salary of $%d", salary);
+    }
+
     public int getSalary() { return this.salary; }
     public LocalDate getHireDate() { return this.hireDate; }
 
@@ -23,9 +24,40 @@ public class Employee {
         salary += raise;
     }
 
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null) return false;
+        if (getClass() != other.getClass()) return false;
+        Employee otherEmployee = (Employee) other;
+
+        return Objects.equals(this.getName(), otherEmployee.getName())
+                && this.salary == otherEmployee.getSalary()
+                && Objects.equals(this.getHireDate(), otherEmployee.getHireDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), salary, hireDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name=" + getName() +
+                ", salary=" + salary +
+                ", hireDate=" + hireDate +
+                '}';
+    }
+
     public static void main(String[] args) {
-        Employee employee = new Employee(null, 10000, LocalDate.of(2023, 1, 15) );
+        Employee employee = new Employee("Jack", 10000, LocalDate.of(2023, 1, 15) );
         System.out.println(employee.getName() + " "
         + employee.getSalary() + " " + employee.getHireDate());
+
+        Employee employee1 = new Employee("Jack", 10000, LocalDate.of(2023, 1, 15) );
+        System.out.println(employee.equals(employee1));
+        System.out.println(String.format("Hash: %d  %d", employee1.hashCode(), employee.hashCode()));
+        System.out.println(employee);
     }
 }
