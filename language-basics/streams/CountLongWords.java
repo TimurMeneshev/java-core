26f725aaa9cba2bb74a0ebdd657a2ee2465aef6e
TimurMeneshev <timmyphone@icloud.com>
2023-05-24 08:00:59 +0700

    optional practice

--- language-basics/streams/CountLongWords.java
+++ language-basics/streams/CountLongWords.java
@@ -1,2 +1,27 @@
-package streams;public class CountLongWords {
+package streams;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLongWords {
+    public static void main(String[] args) throws IOException {
+        var contents = Files.readString(
+                Paths.get("/Users/timurmenesev/Downloads/curl_request_log.txt"));
+        List<String> words = List.of(contents.split("\\PL+"));
+
+        long count = 0;
+        for (String w : words) {
+            if (w.length() > 12) count++;
+        }
+        System.out.println(count);
+
+        count = words.stream().filter(w -> w.length() > 12).count();
+        System.out.println(count);
+
+        count = words.parallelStream().filter(w -> w.length() > 12).count();
+        System.out.println(count);
+    }
 }
--- language-basics/streams/StreamExample.java
+++ language-basics/streams/StreamExample.java
@@ -1,2 +1,23 @@
-package streams;public class StreamExample {
+package streams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamExample {
+    public static void main(String[] args) throws IOException {
+        var logString = Files.readString(Path.of("/Users/timurmenesev/Downloads/curl_request_log.txt"));
+        List<String> lWords = List.of(logString.split("\\PL+"));
+        //фильтрация стрима
+        Stream<String> words = lWords.stream()
+                .filter(w -> w.contains("a"));
+        //words.forEach(System.out::println);
+
+        //преобразование стрима
+        lWords.stream().map(String::toUpperCase).forEach(System.out::println);
+
+        lWords.stream().map(s -> s.concat(s)).forEach(System.out::println);
+    }
 }
