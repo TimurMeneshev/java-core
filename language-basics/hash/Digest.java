6a5d1f46e69e2bfeb00f6510f160fb27adafd757
TimurMeneshev <timmyphone@icloud.com>
2023-08-20 18:50:48 +0700

    security practice

---
+++ language-basics/hash/Digest.java
@@ -0,0 +1,42 @@
+package hash;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.Security;
+import java.util.Scanner;
+
+public class Digest {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        var in = new Scanner(System.in);
+        String filename;
+        if (args.length >= 1) filename = args[0];
+        else {
+            System.out.println("File name: ");
+            filename = in.nextLine();
+        }
+
+        String algname;
+        if (args.length >= 2) algname = args[1];
+        else {
+            System.out.println("Select one of the following algorithms: ");
+            for (Provider p : Security.getProviders())
+                for (Provider.Service s : p.getServices())
+                    if (s.getType().equals("MessageDigest"))
+                        System.out.println(s.getAlgorithm());
+            System.out.print("Algorithm: ");
+            algname = in.nextLine();
+        }
+
+        MessageDigest alg = MessageDigest.getInstance(algname);
+        byte[] input = Files.readAllBytes(Path.of(filename));
+        byte[] hash = alg.digest(input);
+        for (int i = 0; i < hash.length; i++) {
+            System.out.printf("%02X ", hash[i] & 0xFF);
+        }
+        System.out.println();
+    }
+}
